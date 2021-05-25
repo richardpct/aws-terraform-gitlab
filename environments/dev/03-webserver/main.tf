ae60d357c57cad63812de9a748f83daf45e1a2ed
richardpct <noreply>
2021-05-25 19:35:35 +0200

    configuring EFS

--- environments/dev/03-webserver/main.tf
+++ environments/dev/03-webserver/main.tf
@@ -11,7 +11,7 @@ module "webserver" {
   base_remote_state_key        = var.dev_base_key
   database_remote_state_bucket = var.bucket
   database_remote_state_key    = var.dev_database_key
-  instance_type                = "t2.medium"
+  instance_type                = "c5.xlarge"
   image_id                     = "ami-072ec828dae86abe5"  # CentOS 7
   gitlab_pass                  = var.dev_gitlab_pass
   postgres_user                = var.dev_postgres_user
--- modules/webserver/user-data.sh
+++ modules/webserver/user-data.sh
@@ -3,11 +3,10 @@
 set -x
 
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-mkdir /var/opt/gitlab
-echo '${efs_dns_name}:/ /var/opt/gitlab nfs4 vers=4.1,hard,rsize=1048576,wsize=1048576,timeo=600,retrans=2,noresvport 0 2' >> /etc/fstab
-mount /var/opt/gitlab
+mkdir /var/opt/gitlab-nfs
+echo '${efs_dns_name}:/ /var/opt/gitlab-nfs nfs4 vers=4.1,hard,rsize=1048576,wsize=1048576,timeo=600,retrans=2,noresvport 0 2' >> /etc/fstab
+mount /var/opt/gitlab-nfs
 sudo yum -y update
-sudo yum -y upgrade
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | bash
 EXTERNAL_URL="http://${alb_dns_name}" yum install -y gitlab-ee
 cat << EOF >> /etc/gitlab/gitlab.rb
@@ -22,5 +21,10 @@ gitlab_rails['db_host'] = "${postgres_address}"
 redis['enable'] = false
 gitlab_rails['redis_host'] = "${redis_address}"
 gitlab_rails['redis_port'] = 6379
+
+git_data_dirs({"default" => { "path" => "/var/opt/gitlab-nfs/gitlab-data/git-data"} })
+gitlab_rails['uploads_directory'] = '/var/opt/gitlab-nfs/gitlab-data/uploads'
+gitlab_rails['shared_path'] = '/var/opt/gitlab-nfs/gitlab-data/shared'
+gitlab_ci['builds_directory'] = '/var/opt/gitlab-nfs/gitlab-data/builds'
 EOF
 gitlab-ctl reconfigure
